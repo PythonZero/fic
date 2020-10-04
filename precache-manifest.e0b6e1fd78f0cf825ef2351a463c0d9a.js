1ee2af8b1ee9eceae8e1598ad1498626e14f9128
PythonZero <ahammad@outlook.com@users.noreply.github.com>
2020-10-04 06:18:49 +0100

    Updates

--- precache-manifest.e0b6e1fd78f0cf825ef2351a463c0d9a.js
+++ precache-manifest.e0b6e1fd78f0cf825ef2351a463c0d9a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf76ed19e4916923af12e2de61fa94d3",
+    "revision": "efe1b38220736a057266dc8d1b38c31c",
     "url": "/index.html"
   },
   {
-    "revision": "c7fe7eb8368664b5ba93",
+    "revision": "0e6fbe9c9e38b9cddb8b",
     "url": "/static/css/main.045a4c54.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.398e2941.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c7fe7eb8368664b5ba93",
-    "url": "/static/js/main.f1f54dad.chunk.js"
+    "revision": "0e6fbe9c9e38b9cddb8b",
+    "url": "/static/js/main.c45e8abc.chunk.js"
   },
   {
     "revision": "e8feb3bd7fe2e4f7496e",
