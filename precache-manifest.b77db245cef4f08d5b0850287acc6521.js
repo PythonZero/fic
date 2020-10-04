2ff09d41500ac2d351ff67197cd60ebe28238800
PythonZero <ahammad@outlook.com@users.noreply.github.com>
2020-10-04 04:53:24 +0100

    Updates

---
+++ precache-manifest.b77db245cef4f08d5b0850287acc6521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e77123ce30b77e31456a6329c2d894",
+    "url": "/fic/index.html"
+  },
+  {
+    "revision": "32be8d85ba51320272ab",
+    "url": "/fic/static/css/main.045a4c54.chunk.css"
+  },
+  {
+    "revision": "a3f4eb07697b8e0b0cc3",
+    "url": "/fic/static/js/2.5b96b200.chunk.js"
+  },
+  {
+    "revision": "e5276e16ee129bee3389be7c605a3111",
+    "url": "/fic/static/js/2.5b96b200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32be8d85ba51320272ab",
+    "url": "/fic/static/js/main.0985a8b6.chunk.js"
+  },
+  {
+    "revision": "5ce3fafe7874396111c7",
+    "url": "/fic/static/js/runtime-main.1a5d7654.js"
+  },
+  {
+    "revision": "db66fa20a290dce9b74ffac83075f49e",
+    "url": "/fic/static/media/mosque.db66fa20.jpg"
+  }
+]);
