f4c2262d87321ad7ec561cda6738f2302d33188c
PythonZero <ahammad@outlook.com@users.noreply.github.com>
2020-10-09 01:35:11 +0100

    Updates

---
+++ fic/precache-manifest.7478c6478d7d7f905d24ee0807657835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b587d2258417fe38de209656df2bd261",
+    "url": "/fic/index.html"
+  },
+  {
+    "revision": "0479d406d394e400dc0d",
+    "url": "/fic/static/css/main.045a4c54.chunk.css"
+  },
+  {
+    "revision": "043ecb5c4f25d662591b",
+    "url": "/fic/static/js/2.7ad9b2c2.chunk.js"
+  },
+  {
+    "revision": "e5276e16ee129bee3389be7c605a3111",
+    "url": "/fic/static/js/2.7ad9b2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0479d406d394e400dc0d",
+    "url": "/fic/static/js/main.98c799e5.chunk.js"
+  },
+  {
+    "revision": "5ce3fafe7874396111c7",
+    "url": "/fic/static/js/runtime-main.1a5d7654.js"
+  },
+  {
+    "revision": "db66fa20a290dce9b74ffac83075f49e",
+    "url": "/fic/static/media/mosque.db66fa20.jpg"
+  }
+]);
--- precache-manifest.7478c6478d7d7f905d24ee0807657835.js
+++ precache-manifest.7478c6478d7d7f905d24ee0807657835.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "71a7cd509b2c5ea2bd449b0861eaf22d",
+    "revision": "b587d2258417fe38de209656df2bd261",
     "url": "/fic/index.html"
   },
   {
-    "revision": "ba3746952d1c7eb85c2e",
+    "revision": "0479d406d394e400dc0d",
     "url": "/fic/static/css/main.045a4c54.chunk.css"
   },
   {
-    "revision": "af4593429c324a1ce39d",
-    "url": "/fic/static/js/2.398e2941.chunk.js"
+    "revision": "043ecb5c4f25d662591b",
+    "url": "/fic/static/js/2.7ad9b2c2.chunk.js"
   },
   {
     "revision": "e5276e16ee129bee3389be7c605a3111",
-    "url": "/fic/static/js/2.398e2941.chunk.js.LICENSE.txt"
+    "url": "/fic/static/js/2.7ad9b2c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ba3746952d1c7eb85c2e",
-    "url": "/fic/static/js/main.9685f916.chunk.js"
+    "revision": "0479d406d394e400dc0d",
+    "url": "/fic/static/js/main.98c799e5.chunk.js"
   },
   {
     "revision": "5ce3fafe7874396111c7",
